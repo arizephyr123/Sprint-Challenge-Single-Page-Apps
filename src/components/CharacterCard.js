4f773868d3cbea407c9bd19c703724b5e4510cc2
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-09-20 09:12:44 -0700

    List of characers displayed

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import CharacterList from "./CharacterList";
+
 export default function CharacterCard({ character }) {
   const { name } = character;
-  return <span>{character}</span>;
+  return <span>{character.name}</span>;
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,39 +1,42 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-import CharacterCard from "./CharacterCard"
+import CharacterCard from "./CharacterCard";
 
 const CharacterList = props => {
   // TODO: Add useState to track data from useEffectconst [character, setCharacter] = useState([])
   const [character, setCharacter] = useState([]);
-  
-    useEffect(() => {
-      const getCharacters = () => {
-        axios
-          .get('https://rickandmortyapi.com/api/character/')
-          .then(response => {
-            console.log(response.data.results);
-            setCharacter(response.data.results);
-          })
-          .catch(error => {
-            console.error('Server Error', error);
-          });
-      }
-      
-      getCharacters();
-    }, [props]);
+
+  useEffect(() => {
+    const getCharacters = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          console.log(response.data.results);
+          setCharacter(response.data.results);
+        })
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+
+    getCharacters();
+  }, [props]);
 
   return (
-    <section className="character-list"> {character.map(character => (
-      <h2>{character.name}</h2>
-      // <CharacterCard/>
-      // TODO
-    // <Link to={`/movie/${item.id}`}>
-    // <CharacterDetails key={movie.id} movie={movie} />
-    // </Link>
-  ))}
+    <section className="character-list">
+      {" "}
+      {character.map(character => (
+        <div>
+          <Link to={`/character/${character.id}`}>
+            <h2>{character.name}</h2>
+            {/* <CharacterCard/> */}
+          </Link>
+        </div>
+      ))}
     </section>
   );
-}
+};
 
 export default CharacterList;
